aa8a0aa4191a162229de1db8967e72993bd07270
Adam Howard <howarda8@nku.edu>
2014-01-28 02:06:45 +0000

    added functionality to show and add students, as well as showing the pictures associated with the image URLs specified

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,21 @@ class StudentsController < ApplicationController
   end
   
   def create
-    render text: params[:student].inspect
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to @student
+  end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def index
+    @students = Student.all
+  end
+  
+  private
+  def student_params
+    params.require(:student).permit(:name, :nickname, :email, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121233101) do
+ActiveRecord::Schema.define(version: 20140128013531) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20140121233101) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
