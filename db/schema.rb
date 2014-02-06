6f889e843e7d974d6a3d2c21b93ab490dea16f5d
Adam Howard <howarda8@nku.edu>
2014-02-06 23:43:48 +0000

    Sanitize the code that is no longer being used.
    Remove controllers, views, and helpers for session and users.  Also
    wrtte a migration to remove the users table from the database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205225422) do
+ActiveRecord::Schema.define(version: 20140206234234) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -38,13 +38,6 @@ ActiveRecord::Schema.define(version: 20140205225422) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
